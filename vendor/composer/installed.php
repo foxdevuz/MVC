5f85c4e746b73fd5e417fd7248e7eeaf10b3c390
abdullajonov.uz <abdullajonovboburjon05@gmail.com>
2024-05-20 11:36:57 +0500

    [upd] File structure

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'abdullajonov.uz/mvc',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '3ecd852f808162615fcfc9eea665a8d7018ee2ce',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'abdullajonov.uz/mvc' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '3ecd852f808162615fcfc9eea665a8d7018ee2ce',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
